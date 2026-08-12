1960ef1def1423ba3b1e63e234a823526820b693
FitrieSGL <fitrie@siagalabs.com>
2026-08-12 09:21:58 +0800

    chore: remove deadcode

--- src/services/firehydrant/extractExcel.ts
+++ src/services/firehydrant/extractExcel.ts
@@ -2,95 +2,7 @@ import * as ExcelJS from 'exceljs';
 import * as fs from 'fs';
 import { stringify } from 'csv-stringify';
 
-interface FHDataType {
-  id_pili: number;
-  hydrant_id_uuid: string;
-  station_id_uuid: string;
-  station_id: number;
-  station_code: string;
-  zon: string;
-  no_pili: string;
-  pili_num_combine: {
-    result: string;
-    sharedFormula: string;
-    error?: string,
-  };
-  alamat: string;
-  penanda_kawasan: string;
-  id_kedudukan: number;
-  kedudukan: string;
-  lokasi: string;
-  latitud: number;
-  longitud: number;
-  id_negeri: number;
-  state_id_uuid: string;
-  negeri: string;
-  id_daerah: number;
-  daerah: string;
-  id_pemilikan_pili: number;
-  pemilikan_pili: string;
-  id_status_pili: number;
-  status_pili: string;
-  diameter_pengeluaran: number;
-  id_jenis_pili: number;
-  jenis_pili: string;
-  id_parlimen: number;
-  parlimen: string;
-  tarikh_pili: string;
-  id_syarikat_air: number;
-  flag_migrasi: string;
-  id_bandar: number;
-  bandar: string;
-  city_id_uuid: string;
-  latitud_original: string;
-  longitud_original: string;
-  no_pili_asal: number;
-  dun: string | null;
-}
 
-
-// export async function readfileSpbbExcelAndConvertToCSV(
-//   filePath: string,
-//   fileOutputPath: string,
-//   worksheetName: string
-// ) {
-//   const workbook = new ExcelJS.Workbook();
-//   await workbook.xlsx.readFile(filePath);
-
-//   const worksheet = workbook.getWorksheet(worksheetName);
-
-//   const data: FHDataType[] = [];
-//   let headers: any = [];
-
-//   worksheet?.eachRow((row, rowNumber) => {
-//     if (rowNumber === 1) {
-//       // First row as headers
-//       row.eachCell((cell, colNumber) => {
-//         headers[colNumber] = cell.value;
-//       });
-//     } else {
-//       // Data rows
-//       const rowData: any = {};
-//       row.eachCell((cell, colNumber) => {
-//         const header = headers[colNumber];
-//         if (header) {
-//           rowData[header] = cell.value;
-//         }
-//       });
-
-//       // Only add row if it has data
-//       if (Object.keys(rowData).length > 0) {
-//         data.push(rowData);
-//       }
-//     }
-//   });
-
-//   // const modifiedListData = data.filter(item => (item.station_code === 'JHT'));
-//   // const modifiedListData = data.filter(item => (item.dun === 'Kota Anggerik') && (item.station_id !== 11));
-//   // console.log(modifiedListData);
-
-//   await exportResultToCSV(data, fileOutputPath);
-// }
 
 export async function readfileSpbbExcelAndConvertToCSV(
   filePath: string,
@@ -161,49 +73,6 @@ export async function readfileSpbbExcelAndConvertToCSV(
     writeStream.on('error', reject);
   });
 }
-
-
-
-// async function readfileSpbb() {
-//   const workbook = new ExcelJS.Workbook();
-//   const path = 'C:/Users/Fitrie/Desktop/etc-FHIS/extract-actual-data/src/excel-file/SPPB - PJ.xlsx'
-//   await workbook.xlsx.readFile(path);
-
-//   const worksheet = workbook.getWorksheet('Pili');
-
-//   const data: FHDataType[] = [];
-//   let headers: any = [];
-
-//   worksheet?.eachRow((row, rowNumber) => {
-//     if (rowNumber === 1) {
-//       // First row as headers
-//       row.eachCell((cell, colNumber) => {
-//         headers[colNumber] = cell.value;
-//       });
-//     } else {
-//       // Data rows
-//       const rowData: any = {};
-//       row.eachCell((cell, colNumber) => {
-//         const header = headers[colNumber];
-//         if (header) {
-//           rowData[header] = cell.value;
-//         }
-//       });
-
-//       // Only add row if it has data
-//       if (Object.keys(rowData).length > 0) {
-//         data.push(rowData);
-//       }
-//     }
-//   });
-
-
-//   // let listModifiedData_1: any[] = [];
-//   // const listPili_1 = await readCSVListNoPili('C:/Users/Fitrie/Desktop/etc-FHIS/actual-data-fhis/extracted/bbp-p14.csv');
-
-//   // console.log(listModifiedData_1.length, listPili_1.length);
-//   // await exportResultToCSV(listModifiedData_1, 'C:/Users/Fitrie/Desktop/etc-FHIS/extract-actual-data/src/excel-file/result/result-list-pili-2.csv');
-// }
 
 
 
